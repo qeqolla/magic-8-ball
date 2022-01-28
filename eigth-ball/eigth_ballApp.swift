82a7500ec170177e5ecf6459a5a0a66008e566ef
Andrii Kmit <98567886+quqgit@users.noreply.github.com>
2022-01-28 11:36:07 +0200

    Initial Commit

---
+++ eigth-ball/eigth_ballApp.swift
@@ -0,0 +1,17 @@
+//
+//  eigth_ballApp.swift
+//  eigth-ball
+//
+//  Created by Andriy Kmit on 28.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct eigth_ballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
